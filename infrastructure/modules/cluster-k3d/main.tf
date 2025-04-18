9baeae1e61ece2fb85a64a4bca844c21ebe65367
zabronax <zabronax@gmail.com>
2025-04-19 00:39:20 +0200

    fix k3d module taint issue

--- infrastructure/modules/cluster-k3d/main.tf
+++ infrastructure/modules/cluster-k3d/main.tf
@@ -5,7 +5,6 @@ locals {
   config_source_hash  = sha256(local.config_path)
   config_template = templatefile(local.config_path, {
     k3s_version     = var.k3s_version
-    cluster_name    = var.k3d_cluster_name
     server_count    = var.server_count
     agent_count     = var.agent_count
     cluster_ip      = var.k3d_cluster_ip
@@ -20,22 +19,6 @@ resource "local_file" "k3d_config" {
   content  = local.config_template
 }
 
-resource "null_resource" "cluster" {
-  # Be weary of null_resources as you need to manually
-  # track it's dependencies and figure out when those changes.
-  # We are doing it simple here and using the input and output
-  # content hashes of the configuration.
-  triggers = {
-    source_hash = local.config_source_hash
-    config_hash = local_file.k3d_config.content_sha256
-  }
-
-  provisioner "local-exec" {
-    command = "k3d cluster create --config ${local_file.k3d_config.filename}"
-    when    = create
-  }
-}
-
 # Terraform’s declarative model means it doesn’t guarantee access to variable values at destroy time.
 # Because of that, we can't reference var.k3d_cluster_name directly in the destroy provisioner.
 #
@@ -44,9 +27,34 @@ resource "null_resource" "cluster" {
 #
 # As far as I can tell, this is the most idiomatic pattern for this use case.
 # If there's a cleaner approach, I'd love to see it.
-resource "null_resource" "cluster_delete" {
+resource "null_resource" "cluster" {
   for_each = toset([var.k3d_cluster_name])
 
+  # Be weary of null_resources as you need to manually
+  # track it's dependencies and figure out when those changes.
+  # We are doing it simple here and using the input and output
+  # content hashes of the configuration.
+  triggers = {
+    cluster_name = var.k3d_cluster_name
+    source_hash  = local.config_source_hash
+    config_hash  = local_file.k3d_config.content_sha256
+  }
+
+  depends_on = [local_file.k3d_config]
+
+  # Due to how Terraform handles resource lifecycles we have to manually
+  # ensure that k3d clusters are destroyed before we try creating a new one.
+  # It might be better to extract this logic into it's own script, but
+  # due to the low persistence requirements here we are just doing it inline.
+  # For production environments, setup something more durable.
+  provisioner "local-exec" {
+    command = "k3d cluster delete ${each.key} || true && k3d cluster create ${each.key} --config ${local_file.k3d_config.filename}"
+    when    = create
+  }
+
+  # This actually gets exectued after the resource gets created when re-applying
+  # which causes the problem mentioned above. It is still needed for providing
+  # a teardown functionality when executing `terraform destroy`
   provisioner "local-exec" {
     command = "k3d cluster delete ${each.key}"
     when    = destroy
