a948491a2da0d0e6ee8dafb4f45381083a7c67fa
LarsGJobloop <lars.gunnar@jobloop.no>
2025-10-20 14:46:18 +0200

    feat: add GitHub collaborators
    - Add new collaborators "Wolfgang25sfj", "desiree25sfj", and "IvanKalyta2025".

--- project/github-collaborators.tf
+++ project/github-collaborators.tf
@@ -2,3 +2,18 @@ resource "github_repository_collaborator" "lg_private" {
   username   = "zabronax"
   repository = github_repository.github_repository.name
 }
+
+resource "github_repository_collaborator" "wolfgang25sfj" {
+  username   = "Wolfgang25sfj"
+  repository = github_repository.github_repository.name
+}
+
+resource "github_repository_collaborator" "desiree25sfj" {
+  username   = "desiree25sfj"
+  repository = github_repository.github_repository.name
+}
+
+resource "github_repository_collaborator" "ivankalyta2025" {
+  username   = "IvanKalyta2025"
+  repository = github_repository.github_repository.name
+}
